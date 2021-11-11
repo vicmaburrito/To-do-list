44017d99b0b7f94af5a012e3d4aa0d178c3a483c
vicmaburrito <vmaldaraca@gmail.com>
2021-11-10 23:37:12 -0600

    Complete milestone

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
-import _ from 'lodash';
 import './style.css';
 import 'bootstrap';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  // myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  return element;
+const button = document.querySelector('button');
+class Todo {
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
 }
 
-document.body.appendChild(component());
+const todos = [
+  new Todo('Hello world 1', false, 0),
+  new Todo('Hello world 2', false, 1),
+  new Todo('Hello world 5', false, 4),
+  new Todo('Hello world 3', false, 2),
+  new Todo('Hello world 4', false, 3),
+];
+
+function populate() {
+  todos.sort((a, b) => (a.index > b.index ? 1 : -1));
+  todos.forEach((todo) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+    <div class="flex">
+      <div>
+          <input type="checkbox">
+          <span>${todo.description}</span>
+      </div>
+      <span class="material-icons">
+          more_vert
+      </span>
+    </div>
+    <hr>`;
+
+    button.parentElement.insertBefore(li, button);
+  });
+}
+
+window.addEventListener('load', () => {
+  populate();
+});
